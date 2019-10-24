6ef693ffc1cc58207e6cb1ff7e4831f3f8957350
UlrichDinh168 <duong0028@gmail.com>
2019-10-25 02:19:12 +0300

    First version of Budget app

---
+++ app.js
@@ -0,0 +1,62 @@
+//modules are to keep pieces of codes that related to one another together,
+//inside of one separated, independent and organized unit.
+//Each modules are private
+
+let budgetController = (() => {
+    //some code
+    
+})();
+
+let UIController = (() => {
+    
+    let DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    return {
+        getIput: () => {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            }
+        },
+
+        getDOMstrings: () => {
+            return DOMstrings;
+        }
+    }
+})(); 
+
+let controller = ((budgetCtrl, UICtrl) => {
+    
+    var setupEventListener = () => {
+        let DOM = UICtrl.getDOMstrings();
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+        document.addEventListener('keypress', (e) => {
+        if(e.keyCode === 13 || e.which === 13){
+            ctrlAddItem();
+        }
+    })
+    }
+
+    let ctrlAddItem = () => {
+        //Get the filled input data
+        let input = UICtrl.getIput();
+        //Add the item to the budget controller
+        //Add the item to the UI
+        //Calculate the budget
+        //Display the budget on the UI
+    }
+
+    return {
+        init: () => {
+            console.log('App has started');
+            setupEventListener();
+        }
+    }
+})(budgetController, UIController);
+controller.init();
